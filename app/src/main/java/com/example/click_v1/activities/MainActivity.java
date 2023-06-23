7228317a026ab53cac3345bfb16fb9504e626e58
Yujing Chen <yujing.chen@zervant.com>
2023-06-23 21:45:48 +0300

    CLICK-0009 Signout and FMC post and remove in firebase

--- app/src/main/java/com/example/click_v1/activities/MainActivity.java
+++ app/src/main/java/com/example/click_v1/activities/MainActivity.java
@@ -2,14 +2,86 @@ package com.example.click_v1.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.Toast;
 
-import com.example.click_v1.R;
+import com.example.click_v1.databinding.ActivityMainBinding;
+import com.example.click_v1.utilities.Constants;
+import com.example.click_v1.utilities.PreferenceManager;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.HashMap;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private PreferenceManager preferenceManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        preferenceManager = new PreferenceManager(getApplicationContext());
+        loadUserDetails();
+        getToken();
+        setListeners();
+    }
+
+    private void setListeners () {
+        binding.imageSignOut.setOnClickListener(v-> signOut());
+    }
+
+    private void loadUserDetails() {
+        binding.textName.setText(preferenceManager.getString(Constants.KEY_NAME));
+        byte[] bytes = Base64.decode(preferenceManager.getString(Constants.KEY_IMAGE), Base64.DEFAULT);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        binding.imageProfile.setImageBitmap(bitmap);
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void getToken() {
+        // get token from firebase messaging and pass token to updateToken
+        FirebaseMessaging.getInstance().getToken().addOnSuccessListener(this::updateToken);
+    }
+
+    private void updateToken(String token) {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        // get user data from database with collection name and user ID
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USER)
+                .document(preferenceManager.getString(Constants.KEY_USER_ID));
+        // save FCM token to database
+        documentReference.update(Constants.KEY_FCM_TOKEN, token)
+                .addOnSuccessListener(unused -> {})
+                .addOnFailureListener(e -> {});
+    }
+
+    private void signOut() {
+        showToast("Signing out...");
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        // get user data from database with collection name and user ID
+        DocumentReference documentReference = database.collection(Constants.KEY_COLLECTION_USER).document(
+                preferenceManager.getString(Constants.KEY_USER_ID)
+        );
+        // create a map with key fcm token and empty value
+        HashMap<String,Object> updates = new HashMap<>();
+        // empty key fcm token in database
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        documentReference.update(updates).addOnSuccessListener(unused -> {
+            preferenceManager.clear();
+            startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+            finish();
+        }).addOnFailureListener(e -> showToast("Unable to sign out"));
     }
 }
