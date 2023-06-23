f88b47069ad94e2353a6dbd61fa195e196f7cdf1
Yujing Chen <yujing.chen@zervant.com>
2023-06-23 14:38:07 +0300

    CLICK-0006 User signup check, save to firebase and image picker

--- app/src/main/java/com/example/click_v1/activities/SignInActivity.java
+++ app/src/main/java/com/example/click_v1/activities/SignInActivity.java
@@ -2,20 +2,18 @@ package com.example.click_v1.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import com.example.click_v1.R;
 import com.example.click_v1.databinding.ActivitySignInBinding;
-import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
     // automatically generate binding class as configured in
     private ActivitySignInBinding binding;
-
+Z
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,18 +27,6 @@ public class SignInActivity extends AppCompatActivity {
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(), SignUpActivity.class))
         );
-        binding.buttonSignIn.setOnClickListener(v -> addData());
-    }
+    };
 
-    private void addData() {
-        FirebaseFirestore database = FirebaseFirestore.getInstance();
-        HashMap<String, Object> data = new HashMap<String, Object>();
-        data.put("firstname", "Yu");
-        data.put("lastname", "Che");
-        database.collection("users").add(data).addOnSuccessListener(documentReference -> {
-            Toast.makeText(getApplicationContext(), "User data is updated", Toast.LENGTH_SHORT).show();
-        }).addOnFailureListener(exception -> {
-            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
-        });
-    }
 }
--- app/src/main/java/com/example/click_v1/activities/SignUpActivity.java
+++ app/src/main/java/com/example/click_v1/activities/SignUpActivity.java
@@ -1,15 +1,33 @@
 package com.example.click_v1.activities;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.click_v1.R;
 import com.example.click_v1.databinding.ActivitySignUpBinding;
+import com.example.click_v1.utilities.Constants;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
 
 public class SignUpActivity extends AppCompatActivity {
     private ActivitySignUpBinding binding;
+    private String encodedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +40,101 @@ public class SignUpActivity extends AppCompatActivity {
     private void setListeners() {
         // contain direct references to all views that have an ID
         binding.textSignIn.setOnClickListener(v -> onBackPressed());
+        binding.buttonSignUp.setOnClickListener(v -> {
+            if (isValidSignUpDetails()) {
+                signUp();
+            }
+        });
+        binding.layoutImage.setOnClickListener(v->{
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            pickImage.launch(intent);
+        });
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void signUp() {
+        loading(true);
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String,Object> user = new HashMap<>();
+        user.put(Constants.KEY_NAME, binding.inputName.getText().toString());
+        user.put(Constants.KEY_EMAIL, binding.inputEmail.getText().toString());
+        user.put(Constants.KEY_PASSWORD, binding.inputPassword.getText().toString());
+        user.put(Constants.KEY_IMAGE, encodedImage);
+        database.collection(Constants.KEY_COLLECTION_USER)
+                .add(user)
+                .addOnSuccessListener(documentReference -> {})
+                .addOnFailureListener(exception ->{
+                });
+    }
+
+    private String encodeImage(Bitmap bitmap) {
+        int previewWidth = 150;
+        int previewHeight = bitmap.getHeight() * bitmap.getWidth();
+        Bitmap previousBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight, false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        previousBitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(bytes, Base64.DEFAULT);
+    }
+
+    private final ActivityResultLauncher<Intent> pickImage = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if(result.getResultCode() == RESULT_OK){
+                    if(result.getData()!= null) {
+                        Uri imageUri = result.getData().getData();
+                        try {
+                            InputStream inputStream = getContentResolver().openInputStream(imageUri);
+                            Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                            binding.imageProfile.setImageBitmap(bitmap);
+                            binding.textAddImage.setVisibility(View.GONE);
+                            encodedImage = encodeImage(bitmap);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+    );
+
+    private Boolean isValidSignUpDetails() {
+        if (encodedImage == null) {
+            showToast("Select profile image");
+            return false;
+        } else if (binding.inputName.getText().toString().trim().isEmpty()) {
+            showToast("Enter name");
+            return false;
+        } else if (binding.inputEmail.getText().toString().trim().isEmpty()) {
+            showToast("Enter email");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(binding.inputEmail.getText().toString()).matches()) {
+            showToast("Enter valid image");
+            return false;
+        } else if (binding.inputPassword.getText().toString().trim().isEmpty()) {
+            showToast("Enter password");
+            return false;
+        } else if (binding.inputConfirmPassword.getText().toString().trim().isEmpty()) {
+            showToast("Confirm your password");
+            return false;
+        } else if (!binding.inputPassword.getText().toString().equals(binding.inputConfirmPassword.getText().toString())) {
+            showToast("Password and confirm password must be the same");
+            return false;
+        } else {
+            return true;
+        }
+    };
+
+    private void loading(Boolean isLoading) {
+        if(isLoading) {
+            binding.buttonSignUp.setVisibility(View.INVISIBLE);
+            binding.progressBar.setVisibility(View.VISIBLE);
+        } else {
+            binding.progressBar.setVisibility(View.INVISIBLE);
+            binding.buttonSignUp.setVisibility(View.VISIBLE);
+        }
     }
 }
