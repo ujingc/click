4e7e51400bc92eeb6315b27a7990d8ee442ae64a
Yujing Chen <yujing.chen@zervant.com>
2023-06-23 11:07:09 +0300

    CLICK-0004 Sign up and Sign In layout and build binding in activities, xml folder and app icon

--- app/src/main/java/com/example/click_v1/activities/SignInActivity.java
+++ app/src/main/java/com/example/click_v1/activities/SignInActivity.java
@@ -2,15 +2,29 @@ package com.example.click_v1.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.click_v1.R;
+import com.example.click_v1.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+    // automatically generate binding class as configured in
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        // contain direct references to all views that have an ID
+        binding.textCreateNewAccount.setOnClickListener(v ->
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class))
+        );
     }
 }
--- app/src/main/java/com/example/click_v1/activities/SignUpActivity.java
+++ app/src/main/java/com/example/click_v1/activities/SignUpActivity.java
@@ -2,15 +2,25 @@ package com.example.click_v1.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.click_v1.R;
+import com.example.click_v1.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        // contain direct references to all views that have an ID
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
     }
 }
