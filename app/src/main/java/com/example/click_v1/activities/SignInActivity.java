0533dcd226567aa0698a75146daa1c595a0e0384
Yujing Chen <yujing.chen@zervant.com>
2023-06-23 13:54:04 +0300

    CLICK-0005 Firebase firestore connection and add user data

--- app/src/main/java/com/example/click_v1/activities/SignInActivity.java
+++ app/src/main/java/com/example/click_v1/activities/SignInActivity.java
@@ -8,6 +8,9 @@ import android.widget.Toast;
 
 import com.example.click_v1.R;
 import com.example.click_v1.databinding.ActivitySignInBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class SignInActivity extends AppCompatActivity {
     // automatically generate binding class as configured in
@@ -26,8 +29,18 @@ public class SignInActivity extends AppCompatActivity {
         binding.textCreateNewAccount.setOnClickListener(v ->
                 startActivity(new Intent(getApplicationContext(), SignUpActivity.class))
         );
+        binding.buttonSignIn.setOnClickListener(v -> addData());
     }
 
-
-
+    private void addData() {
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        HashMap<String, Object> data = new HashMap<String, Object>();
+        data.put("firstname", "Yu");
+        data.put("lastname", "Che");
+        database.collection("users").add(data).addOnSuccessListener(documentReference -> {
+            Toast.makeText(getApplicationContext(), "User data is updated", Toast.LENGTH_SHORT).show();
+        }).addOnFailureListener(exception -> {
+            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
+        });
+    }
 }
